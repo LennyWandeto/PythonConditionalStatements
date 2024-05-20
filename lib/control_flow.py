dad0e9e4916826523d26d06f1597103cfa210515
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-05-20 17:36:19 +0300

    Update control_flow.py

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,39 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
+    if  password == "12345" and (username == 'admin' or username =='ADMIN') :
+        return "Access granted"
+    else:
+        return "Access denied"
     pass
 
 def hows_the_weather(temperature):
-    # your code here
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif 40 <= temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
     pass
 
 def fizzbuzz(num):
-    # your code here
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
     pass
 
 def calculator(operation, num1, num2):
-    # your code here
+    try:
+        quotient = f"{num1}{operation}{num2}"
+        return eval(quotient)
+    except:
+        print("Invalid operation!")
+        return None
     pass
